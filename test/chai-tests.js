e43b80e308c3d1d9287000a2bdee512ef852e8ad
novdima-github <bbs.minsk@gmail.com>
2020-06-15 23:48:48 +0300

    Add Pen.js class

---
+++ test/chai-tests.js
@@ -0,0 +1,7 @@
+const chai = require('chai');
+const expect = chai.expect;
+describe('example with assertions', function () {
+    it('equal test with Chai', function () {
+        expect(1).to.be.equal(1)
+    })
+});
